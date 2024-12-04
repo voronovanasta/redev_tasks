a9a23b6f46cff075e1f7e51c6ceb7379d54a0cb7
anastasiya voronova <buchneva.anastasya92@gmail.comm>
2024-12-04 18:59:28 +0300

    feat: add code for 3 assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
+import { useRef } from 'react';
 import './App.css'
-import { LifecycleComponent, FuncComponent} from './components/LifecycleComponent'
+import { List} from './components/List'
 
 
-
+const data = [{task:'Task 1', id: 1}, {task:'Task 2', id: 2}, {task:'Task 3', id: 3}]
 function App() {
-  const style = {width: '300px', display: "flex", flexDirection: "column", justifyContent: 'space-between'}
+  const style = {width: '300px', display: "flex", flexDirection: "column", justifyContent: 'space-between'};
+
+  const inputRef = useRef(null);
+
+  function clickHandler () {
+    inputRef.current.focus();
+  }
   return (
     <div style = {style}>
-      <LifecycleComponent/>
-      <FuncComponent/>
+      <List data={data}/>
+      <input ref ={inputRef} placeholder='Type here...'></input>
+      <button onClick={clickHandler}>Focus</button>
     </div>
   )
 }
